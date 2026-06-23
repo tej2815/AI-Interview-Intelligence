93080ca430e781bb15b0f987d55c8453955cea80
Teja <thejanarayanbandaru15@gmail.com>
2026-06-23 17:36:09 +0530

    Fixed database config

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from app.database.models import Base
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_npdfl13UIrwv@ep-snowy-union-atmykfq7-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -5,7 +5,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_npdfl13UIrwv@ep-snowy-union-atmykfq7-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
