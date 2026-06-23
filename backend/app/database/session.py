510c554830c5e3ed5507791a51efc75594ebe254
Teja <thejanarayanbandaru15@gmail.com>
2026-06-23 17:10:22 +0530

    Fixed database configuration for deployment

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/ai_interview_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_npdfl13UIrwv@ep-snowy-union-atmykfq7-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 engine = create_engine(DATABASE_URL)
 
